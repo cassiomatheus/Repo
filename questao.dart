67ba3edd00ada27f1c53e5d48ddc0e872d632ca7
Cassio Matheus <37419297+cassiomatheus@users.noreply.github.com>
2021-05-13 15:35:49 -0300

    Alterações

---
+++ questao.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Quest extends StatelessWidget {
+  
+  final String texto;
+
+  Quest(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsetsDirectional.all(10) ,
+      child: Text(
+        texto,
+        style:TextStyle(fontSize:21 ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
